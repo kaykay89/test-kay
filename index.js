2952721c93f5b8f34c6c964f9ff2154e3ea73530
kaykay89 <kaylalouis@hotmail.com>
2019-10-05 17:06:32 -0400

    port changé

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Client({
 	user:"iqzrzazsdxbmbw",
 	password: "ad7beb25841c01cdee822e2fe5521e4405fefdfdc6cb0f3624157ab6f100d204",
 	host: "ec2-54-197-239-115.compute-1.amazonaws.com",
-	port: 5432,			
+	port: process.env.PORT || 5432,			
 	database: "dcd20rka0b0q59",
 	ssl:true
 })
