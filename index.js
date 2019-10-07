31c3b1bc777d112113c6771c9612ec86068768f5
kaykay89 <kaylalouis@hotmail.com>
2019-10-07 16:19:53 -0400

    changement connexion postgresql

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const Client = require('pg').Client 
+const Pool = require('pg').Pool 
 //ou écrire: const {Client} = require('pg')  C LA MEME CHOSE
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000
 app.use(express.json())
 
-const client = new Client({
-	user:"iqzrzazsdxbmbw",
-	password: "ad7beb25841c01cdee822e2fe5521e4405fefdfdc6cb0f3624157ab6f100d204",
-	host: "ec2-54-197-239-115.compute-1.amazonaws.com",
-	port: 5432,			
-	database: "dcd20rka0b0q59",
+
+const pool = new Pool ({
+	connectionString: process.env.DATABASE_URL,
 	ssl:true
 })
 
@@ -44,8 +41,7 @@ app.post('/membres', async (req, res) => {
 app.delete('/membres', async (req, res) => {
 	let result = {}
 
-	try{
-	
+	try{	
 		const reqJson = req.body;
 		await deleteMembres(reqJson.nom);
 		result.success = true;
@@ -78,44 +74,56 @@ async function start() {
 	console.log(`Suppression est ${succesDelete}`)
 	*/
 	
+	const membres = await deleteNull();
+	console.log("Suppression des NULL")
+	
 }	
 	
-	async function connect() {
-		try {
-			await client.connect();
-		} catch(e) {
-			console.error(`Erreur de connexion ${e}`)
-		}
+	
+async function connect() {
+	try {
+		await client.connect();
+	} catch(e) {
+		console.error(`Erreur de connexion ${e}`)
 	}
+}
 	
-	async function readMembres() {
-		try {
-			const results = await client.query("select * from famille")
-			return results.rows
-		} catch(e) {
-			return [];
-		}
+async function readMembres() {
+	try {
+		const results = await client.query("select * from famille")
+		return results.rows
+	} catch(e) {
+		return [];
 	}
+}
 	
-	async function createMembres(nom, lien) {
-		try {
-	
+async function createMembres(nom, lien) {
+	try {
 		await client.query("insert into famille values ($1, $2)", [nom, lien]);
 		return true
-		} catch(e){
-			return false;
-		}
-		
-	}
+	} catch(e){
+		return false;
+	}		
+}
 	
-	async function deleteMembres(nom) {
-		try {
-			await client.query("delete from famille where nom = $1", [nom])
-			return true
-		} catch(e) {
-			return false;
-		}
+async function deleteMembres(nom) {
+	try {
+		await client.query("delete from famille where nom = $1", [nom])
+		return true
+	} catch(e) {
+		return false;
 	}
+}
+
+async function deleteNull() {
+	try {
+		await client.query("delete from famille where nom is null")
+		return true
+	} catch(e) {
+		return false;
+	}
+}
+
 	
 
 
